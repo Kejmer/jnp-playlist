4ee80b0a1ef7cda7cfc3494115a0eea53532b156
Kejmer <kajetanhus@gmail.com>
2020-01-11 01:59:31 +0100

    pozna noc

--- Player.h
+++ Player.h
@@ -1,10 +1,21 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include <set>
+#include "Playable.h"
+#include "Playlist.h"
+#include "File."
+
 class Player
 {
+private:
+  unordered_map<std::string> _playlists;
+
 public:
-//TODO wstawić wszystkie funkcje t
+  //Fabryka w środku
+  Playable openFile(File file) //error gdy błąd w treści, jakiś validate by się przydał
+
+  Playlist createPlaylist(std::string name); //error gdy się powtarza nazwa
 };
 
 #endif /* PLAYER_H */
--- Playlist.h
+++ Playlist.h
@@ -5,8 +5,21 @@
 
 class Playlist : public Playable
 {
+private:
+  //tu wskaźnik na klasę reprezentującą strukturkę czy co tam wymyśliłeś
+  //tryb
 public:
   void play() const override;
+
+  void add(element);
+
+  void add(element, position); //rzucamy wyjątek gdy za daleko chcemy wrzucić (?) do ustalenia
+
+  void remove(); //rzucamy wyjątek gdy puste
+
+  void remove(position); //rzucamy wyjątek gdy nie ma tej pozycji
+
+  // void setMode(mode); //rzucamy wyjątek gdy zły tryb
 };
 
 #endif /* PLAYLIST_H */
