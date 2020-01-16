270eeaaa3b6ab3c7db8a7f700e25a93964a8b260
Kejmer <kajetanhus@gmail.com>
2020-01-16 21:08:39 +0100

    encryption:

--- Encryption.cc
+++ Encryption.cc
@@ -1,15 +1,18 @@
-#include "Encription.h"
+#include "Encryption.h"
+
 
 char ROT13::translate(char c)
 {
-  if ("A" <= c && c <= "Z")
-    return this->output[int(c-"A")];
-  if ("a" <= c && c <= "z") {
-    int position = "Z" - "A" + 1 + c - "a";
-    return this->output[position];
+  if ('A' <= c && c <= 'Z')
+    return TRANS_KEY[int(c-'A')];
+  if ('a' <= c && c <= 'z') {
+    int position = 27 + c - 'a';
+    return TRANS_KEY[position];
   }
   return c;
 }
+
+const std::string ROT13::TRANS_KEY = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 std::string ROT13::encode(const std::string &msg)
 {
